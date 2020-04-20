4df5689f76161ef2d50c1678ecffd2355fbfc27c
Zidan <zidancock1@gmail.com>
2020-04-21 04:50:09 +0700

    Upload projek vue

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderBoons />
+    <HeroBoons />
+    <WomanBoons />
+    <InstaBoons />
+    <PartnerBoons />
+    <FooterBoons />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderBoons from '@/components/HeaderBoons.vue'
+import HeroBoons from '@/components/HeroBoons.vue'
+import WomanBoons from '@/components/WomanBoons'
+import InstaBoons from '@/components/InstaBoons'
+import PartnerBoons from '@/components/PartnerBoons'
+import FooterBoons from '@/components/FooterBoons'
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderBoons,
+    HeroBoons,
+    WomanBoons,
+    InstaBoons,
+    PartnerBoons,
+    FooterBoons
+  }
+}
+</script>
